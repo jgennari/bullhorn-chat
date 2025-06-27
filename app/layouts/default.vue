1f90668626db1cb1e8d895cff92dc4d263ef3d68
Joey Gennari <joey@gennari.com>
2025-06-27 18:41:43 -0400

    Replace click event with 'to' prop for Bullhorn authentication link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -156,7 +156,7 @@ defineShortcuts({
           color="neutral"
           variant="ghost"
           class="w-full"
-          @click="navigateTo('/auth/bullhorn')"
+          to="/auth/bullhorn"
         />
       </template>
     </UDashboardSidebar>
